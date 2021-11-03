818088ac07cfc8b3a699c9c68cd3842e04ba175e
James OConnor <jamesedoconnor@gmail.com>
2021-11-03 23:43:55 +0000

    removed all __pycache__

--- website/website/dev_settings.py
+++ website/website/dev_settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY=os.getenv("DJANGO_SECRET_KEY")
 
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: do not run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS_DEV')]
