baee6cdf917359dfc5bfb97a2b0c958d68f46a89
James OConnor <jamesedoconnor@gmail.com>
2021-11-04 05:27:15 +0000

    Added static root

--- website/website/prod_settings.py
+++ website/website/prod_settings.py
@@ -121,6 +121,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+  os.path.join(BASE_DIR, 'static_root'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
