fe0a177a684239f35b0883fcb5e56c80f0f59757
EC2 Default User <ec2-user@ip-172-31-20-120.us-west-2.compute.internal>
2021-11-09 22:15:40 +0000

    Running on NGINX and uwsgi

--- website/website/prod_settings.py
+++ website/website/prod_settings.py
@@ -119,9 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/website/static/'
+STATIC_URL = '/static/'
 
-STATIC_ROOT = "/var/www/resale.com/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+MEDIA_ROOT = '/home/ec2-user/media'
 
 
 # Default primary key field type
